6510fe404ae5b0dbbf7e69b116f2d81be700267d
ValeriyaProkopchenko <val.prokopchenko@gmail.com>
2024-12-27 00:13:55 +0300

    Delete task1.py

--- task1.py
+++
@@ -1,7 +0,0 @@
-distance = float(input("Введите расстояние в километрах: "))
-time = float(input("Введите время в часах: "))
-if time <= 0:
-    print("Время должно быть больше нуля.")
-else:
-    speed = distance / time
-    print(f"Скорость: {speed:.2f} км/ч.")
